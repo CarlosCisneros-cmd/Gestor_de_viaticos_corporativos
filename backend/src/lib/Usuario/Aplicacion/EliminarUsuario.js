647ce828c1c553f4334f4d7a74ac79f5f9977879
336-Daniel <e0850405309@uisrael.edu.ec>
2026-05-13 16:36:06 -0500

    Separacion de front y back y utilizacion de React

---
+++ backend/src/lib/Usuario/Aplicacion/EliminarUsuario.js
@@ -0,0 +1,21 @@
+class EliminarUsuario {
+  constructor(IUsuarioRepositorio) {
+    this.IUsuarioRepositorio = IUsuarioRepositorio;
+  }
+
+  async ejecutar(id) {
+    const resultado = await this.IUsuarioRepositorio.delete(id);
+
+    if (resultado === 0) {
+      const error = new Error("No se pudo eliminar: Usuario no encontrado");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    return { message: "Usuario eliminado correctamente", id };
+  }
+}
+
+module.exports = EliminarUsuario;
+
+
